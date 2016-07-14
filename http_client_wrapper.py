99117bc1eafe45c372ac5b44e69b2825b0c30120
behner <behner@cs.uni-bonn.de>
2016-07-14 10:39:44 +0200

    Bug fixed: use the right HTTP header field

--- http_client_wrapper.py
+++ http_client_wrapper.py
@@ -3,7 +3,7 @@ import requests
 import logging
 
 logger = logging.getLogger('HTTPClientWrapper')
-logger.setLevel(logging.WARN)
+logger.setLevel(logging.INFO)
 
 DEFAULT_HEADERS = {'content-type': 'application/json'}
 
@@ -18,28 +18,32 @@ def _check_response_for_403(response, key):
         raise APIKeyRejected()
 
 
+def _add_api_key(header, api_key):
+    header['X-API-KEY'] = api_key
+    logger.info('HEADER: {}'.format(header))
+
 class HTTPClientWrapper():
 
     def post_json(url, post_data, api_key):
-        DEFAULT_HEADERS['api key'] = api_key
+        _add_api_key(DEFAULT_HEADERS, api_key)
         r = requests.post(url, data=json.dumps(post_data), headers=DEFAULT_HEADERS)
         _check_response_for_403(r, api_key)
         return r
 
     def get(url, api_key):
-        DEFAULT_HEADERS['api key'] = api_key
+        _add_api_key(DEFAULT_HEADERS, api_key)
         r = requests.get(url, headers=DEFAULT_HEADERS)
         _check_response_for_403(r, api_key)
         return r
 
     def get_stream(url, api_key):
-        DEFAULT_HEADERS['api key'] = api_key
+        _add_api_key(DEFAULT_HEADERS, api_key)
         r = requests.get(url, stream=True, headers=DEFAULT_HEADERS)
         _check_response_for_403(r, api_key)
         return r
 
     def delete(url, api_key):
-        DEFAULT_HEADERS['api key'] = api_key
+        _add_api_key(DEFAULT_HEADERS, api_key)
         r = requests.delete(url, headers=DEFAULT_HEADERS)
         _check_response_for_403(r, api_key)
         return r
