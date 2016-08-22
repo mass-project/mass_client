6576975fdd4e5987eb698c4a3e9d4a0673c9d112
Fabian Rump <rumpf@cs.uni-bonn.de>
2016-08-22 13:51:05 +0200

    Trying dependency links for own library code.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import os
 import subprocess
 from setuptools import setup, find_packages
 
-requirements = [
-        'requests',
-        'common-helper-encoder',
-        ]
-
 setup(
     name="mass_client",
     version=subprocess.check_output(['git', 'describe', '--always'], cwd=os.path.dirname(os.path.abspath(__file__))).strip().decode('utf-8'),
     packages=find_packages(),
+    install_requires=[
+        'requests>=2.11.1',
+        'common-helper-encoder==0.1',
+    ],
+    dependency_links=['git+ssh://git@github.com/mass-project/common_helper_encoder.git#egg=common_helper_encoder-0.1']
 )
