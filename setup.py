b089e9acc8cc684fdb922f70450943660f834108
Timm Behner <behner@fkie.fraunhofer.de>
2017-03-02 13:06:33 +0100

    Removed http_client and updated setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,5 @@ setup(
     name="mass_client",
     version=subprocess.check_output(['git', 'describe', '--always'], cwd=os.path.dirname(os.path.abspath(__file__))).strip().decode('utf-8'),
     packages=find_packages(),
-    install_requires=[
-        'requests>=2.11.1',
-        'common-helper-encoder==0.1',
-    ],
-    dependency_links=['git+ssh://git@github.com/mass-project/common_helper_encoder.git#egg=common_helper_encoder-0.1']
+    dependency_links=['https://github.com/mass-project/mass_api_client.git']
 )
